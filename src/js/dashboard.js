35d47db309102f0f00dcd89739145c7048cc761a
Sebastian Linero De Castro <sebastianlinero15@gmail.com>
2025-07-09 19:38:31 -0500

    feat:add

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -10,3 +10,12 @@
 
 //darle funcionalidad al boton de cerrar sesion
 //implemenrtar un guardian si tienes una sesion inciada que te mande al dashboard
+
+//administrador una nueva vista, ver todas las mascotas
+//sin
+
+//asycronismo 
+//json server
+//mantener sesion
+//seguridad de sesion
+//persintensia 
